b86145d052b78baf1ab0e46057c3b9f60d6fd0ac
Alex Taffe <alex.taffe@gmail.com>
2017-09-19 12:15:38 -0400

    Begin creating shared code repository

---
+++ Dining Tracker Shared/DiningTracker.h
@@ -0,0 +1,13 @@
+//
+//  DiningTracker.h
+//  Dining Tracker
+//
+//  Created by Alex Taffe on 9/19/17.
+//  Copyright © 2017 Alex Taffe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiningTracker : NSObject
+
+@end
---
+++ Dining Tracker Shared/DiningTracker.m
@@ -0,0 +1,13 @@
+//
+//  DiningTracker.m
+//  Dining Tracker
+//
+//  Created by Alex Taffe on 9/19/17.
+//  Copyright © 2017 Alex Taffe. All rights reserved.
+//
+
+#import "DiningTracker.h"
+
+@implementation DiningTracker
+
+@end
